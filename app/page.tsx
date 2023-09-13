7417d9a37eb780353b376badb9c47267506fc3c2
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-09-13 03:11:25 +0300

    table, button

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ import Table from '@/components/Table';
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen w-screen">
-      <div className="w-full max-w-6xl h-5/6 rounded-xl overflow-hidden bg-gray-200">
+      <div className="w-full max-w-6xl h-5/6 rounded-xl overflow-hidden bg-gray-200 mx-4 p-1 flex flex-col justify-between">
       <SearchBar />
+       <Table />
+       <button className="rounded-b-lg w-full mt-auto font-medium text-gray-600 uppercase text-xl py-4">LOAD MORE</button>
       </div>
-      
     </main>
   )
 }
