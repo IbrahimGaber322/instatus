a13124935028973d4df9e9da8ca2095f5c1c880f
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-09-19 20:06:11 +0300

    fix

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -4,6 +4,7 @@ import { FetchedEventType } from "@/constants/types";
 const Table = ({ events, filterMenuItems }: { events: FetchedEventType[]; filterMenuItems:{name:string, checked:boolean}[] }) => {
   const headings: string[] = ["ACTOR", "ACTION", "DATE"];
   console.log(events);
+
   return (
     <>
       {events.length > 0 ? (
@@ -28,14 +29,14 @@ const Table = ({ events, filterMenuItems }: { events: FetchedEventType[]; filter
                   id={data.id}
                   object={data.object}
                   actor_id={data.actor_id}
-                  action={data.action[0]}
+                  action={data.action[0]||data.action}
                   actor_name={data.actor_name}
                   group={data.group}
                   target_id={data.target_id}
                   target_name={data.target_name}
                   location={data.location}
                   occurred_at={data.occurred_at}
-                  metadata={data.metadata[0]}
+                  metadata={data.metadata[0]||data.metadata}
                   filterMenuItems={filterMenuItems}
                 />
               ))}
